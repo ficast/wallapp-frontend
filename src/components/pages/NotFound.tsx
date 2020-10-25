9564b715f983461cd91ab2856ffcb09aeb0dd197
Filipe de Castro <filipeyoga@gmail.com>
2020-10-25 11:47:22 -0300

    feat: create notfound component

---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement } from "react";
+import { RiErrorWarningFill as Error } from "react-icons/ri";
+import styled from "styled-components";
+import theme from "../../theme/nice";
+import Header from "../organisms/Header";
+
+function NotFound(): ReactElement {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Error size={"20em"} color={theme.colors.primary[200]} />
+        <Title>Page Not Found</Title>
+      </Container>
+    </>
+  );
+}
+
+const Title = styled.h1`
+  color: ${theme.colors.primary[200]};
+`;
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  margin: auto;
+  width: 20em;
+  flex-direction: column;
+`;
+
+export default NotFound;
