fff4249e0bd78e552d005cfd285a41edbc68fe39
Filipe de Castro <filipeyoga@gmail.com>
2020-10-25 11:48:58 -0300

    feat: api methods to create and authenticate users

--- src/components/services/Api.tsx
+++ src/components/services/Api.tsx
@@ -2,15 +2,32 @@ import axios from "axios";
 
 const API_BASE_URL = "http://localhost:3000";
 
+type createUserParams = {
+  name: string;
+  password: string;
+  email: string;
+};
+
+type authUserParams = {
+  password: string;
+  email: string;
+};
+
 export default class Api {
-  static createUser = () => {
+  static createUser = async (params: createUserParams) => {
+    await axios.post(`${API_BASE_URL}/user`, params);
   };
 
-  // static loginAuth = ({password, email}) => {
-  // };
+  static authUser = async (params: authUserParams) => {
+    const response = await axios.post(
+      `${API_BASE_URL}/user/authenticate`,
+      params
+    );
+    return response.data;
+  };
 
-  static getPosts = async (): Promise<any> => {
-    const response = await axios.get(`${API_BASE_URL}/post/`);
+  static getPosts = async (page: number = 0): Promise<any> => {
+    const response = await axios.get(`${API_BASE_URL}/post/?page=${page}`);
     return response.data;
   };
 }
